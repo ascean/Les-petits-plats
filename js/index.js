aa248b7d92c49e41999cfc7ba625b60412582f59
ascean <106957442+ascean@users.noreply.github.com>
2022-08-25 22:19:21 +0200

    intégration complète

--- js/index.js
+++ js/index.js
@@ -1,41 +1,25 @@
 const navItems = document.querySelectorAll(".nav-item")
 
+/**
+ * survol du filtre :
+ *      affichage de l'input avec le placeholder
+ *      élargissement de la zone de filtre
+ * @param {object} navItem élément li
+ * @param {string} action over ou leave
+ */
 var displayFilter = (navItem, action) => {
 
-    const labelFilter = navItem.querySelector(".nav-link")
     const inputFilter = navItem.querySelector(".form-control")
     
-    //survol du filtre :
-    //  affichage de l'input avec le placeholder
-    //  libellé par défaut invisible
-    //  élargissement de la zone de filtre
-    //  agrandissement hauteur en fonction du nombre de dropdown-menu
     if (action === "over") {
         
-        labelFilter.style.opacity = "0"
         inputFilter.classList.remove("d-none")
-        
-        navItem.classList.replace("col-2", "col-6")
-        navItem.classList.remove("default-filter-height")
-        const dropdownItems = navItem.querySelectorAll(".dropdown-item")
-        
-        let nbdropdownItem = dropdownItems.length
-        if (nbdropdownItem === 0) {
-            nbdropdownItem = 1
-        }
-        nbdropdownItem = nbdropdownItem * 50
-        navItem.style.setProperty("height", `${nbdropdownItem}px`)
+        navItem.classList.replace("col-lg-2", "col-lg-6")
 
     } else {
-        
-        labelFilter.style.opacity = "1"
-        
+            
         inputFilter.classList.add("d-none")
-        
-        navItem.classList.replace("col-6", "col-2")
-        navItem.classList.add("default-filter-height")
-        navItem.style.removeProperty('height')
-
+        navItem.classList.replace("col-lg-6", "col-lg-2")
     }
 }
 
