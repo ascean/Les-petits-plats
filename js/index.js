b23ec7c6f1cf13f192487d0b8d512a906c4b05b1
ascean <106957442+ascean@users.noreply.github.com>
2022-08-26 12:26:05 +0200

    menu large avec bootstrap revu

--- js/index.js
+++ js/index.js
@@ -1,36 +1,68 @@
+const navLinks = document.querySelectorAll(".nav-link")
 const navItems = document.querySelectorAll(".nav-item")
 
 /**
- * survol du filtre :
+ * survol du nav-item ou clic sur nav-link (navigation au clavier)
  *      affichage de l'input avec le placeholder
  *      élargissement de la zone de filtre
- * @param {object} navItem élément li
- * @param {string} action over ou leave
+ * @param {DOMElement} navItem élément li
+ * @param {string} action open ou close
  */
-var displayFilter = (navItem, action) => {
+var displayMenu = (navItem, action) => {
 
     const inputFilter = navItem.querySelector(".form-control")
     
-    if (action === "over") {
+    if (action === "open") {
         
         inputFilter.classList.remove("d-none")
+        navItem.classList.replace("rounded","rounded-top")
         navItem.classList.replace("col-lg-2", "col-lg-6")
-
-    } else {
-            
+        
+    }else{
+        
         inputFilter.classList.add("d-none")
+        navItem.classList.replace("rounded-top","rounded")
         navItem.classList.replace("col-lg-6", "col-lg-2")
+        
     }
 }
 
+/**
+ * Retour à l'état initial pour lemenu précédemment ouvert
+ * @param {DOMElement} newNavItem menu sur lequel arrive le focus 
+ */
+var resizeMenu = (newNavItem) => {
+
+    navItems.forEach(navItem => {
+        if (navItem != newNavItem && navItem.classList.contains("show")) {
+            navItem.querySelector(".form-control").classList.add("d-none")
+            navItem.classList.replace("rounded-top","rounded")
+            navItem.classList.replace("col-lg-6", "col-lg-2")
+            navItem.children[2].classList.remove("show")
+        }
+    });
+
+}
+
+//--------------------------------------------- LISTENERS MENU ----------------------------------------
 navItems.forEach(navItem => {
     navItem.addEventListener("mouseover", () => {
-        displayFilter(navItem, "over")
+        displayMenu(navItem, "open")
     })
-    navItem.addEventListener("focus", () => {
-        displayFilter(navItem, "over")
-    })
+
     navItem.addEventListener("mouseleave", () => {
-        displayFilter(navItem, "leave")
+        displayMenu(navItem, "close")
+    })
+})
+
+//Pour gestion de la navigation au clavier
+navLinks.forEach(navLink => {
+    navLink.addEventListener("click", () => {
+        const navItem = navLink.parentElement
+        displayMenu(navItem,"open")
+    })
+    navLink.addEventListener("focus", () => {
+        const navItem = navLink.parentElement
+        resizeMenu(navItem)
     })
 })
